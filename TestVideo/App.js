eae41365f8d3661f50823355d7ad356e629c6dda
Martin Meiring <martin.meiring@entelect.co.za>
2018-05-29 16:22:21 +0200

    Android starter project

--- TestVideo/App.js
+++ TestVideo/App.js
@@ -1,103 +1,23 @@
 import React from 'react';
-import {StyleSheet, Text, View, Dimensions, TouchableOpacity} from 'react-native';
-import  Video, { Downloader }  from './library/index';
-
-const {width, height} = Dimensions.get('window');
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            muted: false,
-            width: width,
-            height: width / (16 / 9),
-            showPlayer: false,
-            progress: 0,
-            play: false
-        };
-
-        this.onDownloadProgress = this.onDownloadProgress.bind(this);
-        this.showVideo = this.showVideo.bind(this);
-        this.downloader = new Downloader({
-            onDownloadProgress: this.onDownloadProgress,
-            onDownloadError: (data) => console.log(data),
-            onDownloadStarted: (data) => console.log(data),
-            onDownloadFinished: (data) => console.log(data),
-            onDownloadCanceled: (data) => console.log(data)
-        });
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <TouchableOpacity onPress={() => {this.downloader.downloadStream('https://d2h2jy22itvgms.cloudfront.net/hls/269149/trailer.m3u8', '269149')}}>
-                    <Text>setupAssetDownload()</Text>
-                </TouchableOpacity>
-                <Text>{ this.state.progress}%</Text>
-                <TouchableOpacity onPress={() => {this.setState({showPlayer: true});}}>
-                    <Text>showVideo()</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => {this.downloader.deleteDownloadedStream('269149')}}>
-                    <Text>deleteDownloadedStream()</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => {this.downloader.pauseDownload('269149')}}>
-                    <Text>pauseDownload()</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => {this.downloader.resumeDownload('269149')}}>
-                    <Text>resumeDownload()</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => {this.downloader.cancelDownload('269149')}}>
-                    <Text>cancelDownload()</Text>
-                </TouchableOpacity>
-                { this.showVideo() }
-            </View>
-        );
-    }
-
-    showVideo() {
-        if (this.state.showPlayer) {
-            return (
-                <View>
-                <Video
-                    ref={(ref) => {
-                        this.player = ref
-                    }}
-                    style={{width: this.state.width, height: 190, backgroundColor: 'black'}}
-                    autoplay={true}
-                    preload='auto'
-                    loop={true}
-                    muted={this.state.muted}
-                    src="269149"
-                />
-                <TouchableOpacity onPress={() => {
-                    if (this.state.play) {
-                        this.setState({play: false});
-                        this.player.pause();
-                    } else {
-                        this.setState({play: true});
-                        this.player.play();
-                    }
-                }}>
-                    <Text>play()</Text>
-                </TouchableOpacity>
-                </View>
-            );
-        }
-        return null;
-    }
-
-    onDownloadProgress(data) {
-        console.log(data);
-        this.setState({progress:data.percentComplete});
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
--- TestVideo/library/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import MediaPlayerView from './media-player/media-player-view';
-import Downloader from './media-downloader/media-downloader';
-
-export default MediaPlayerView;
-export { Downloader };
