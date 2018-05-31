fc2b61bb3c6331ed0a563dddc71cf5bae2502b04
Daniel Lotz <daniel.lotz@entelect.co.za>
2018-05-31 15:07:30 +0200

    remove library first

--- deployBitbuchet.sh
+++ deployBitbuchet.sh
@@ -3,7 +3,8 @@ VERSION='1.0.'$1
 git checkout -b "release-"$VERSION
 git checkout "release-"$VERSION
 git push origin "release-"$VERSION
-cp -R TestVideo/library /
+rm -rf library
+cp -R TestVideo/library library
 rm -rf TestVideo/
 rm ./deployBitbuchet.sh
 git add .
