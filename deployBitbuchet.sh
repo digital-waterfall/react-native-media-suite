19c8d82336d06a5cb677c3364135e03face84de0
Craig van Heerden <u15029779@tuks.co.za>
2018-05-31 15:25:31 +0200

    Whole version number

--- deployBitbuchet.sh
+++ deployBitbuchet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION='1.0.'$1
+VERSION=$1
 git checkout -b "release-"$VERSION
 git checkout "release-"$VERSION
 git push origin "release-"$VERSION
@@ -10,3 +10,4 @@ rm ./deployBitbuchet.sh
 git add .
 git commit -m 'Created release-'$VERSION
 git push --set-upstream origin "release-"$VERSION
+git checkout master
