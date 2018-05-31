93aa1f77ed6bde2d67cc0f01c9d0a9139a862615
Daniel Lotz <daniel.lotz@entelect.co.za>
2018-05-31 14:58:07 +0200

    fixed script

--- deployBitbuchet.sh
+++ deployBitbuchet.sh
@@ -3,7 +3,7 @@ VERSION='1.0.'$1
 git checkout -b "release-"$VERSION
 git checkout "release-"$VERSION
 git push origin "release-"$VERSION
-cp -R TestVideo/library library
+cp -R TestVideo/library /
 rm -rf TestVideo/
 rm ./deployBitbuchet.sh
 git add .
