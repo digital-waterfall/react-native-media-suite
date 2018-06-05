a8ffbfdb8f560f9a35dd58d8e958ab95e3339029
Martin Meiring <martin.meiring@entelect.co.za>
2018-06-05 16:23:47 +0200

    Downloader in progress.

--- TestVideo/App.js
+++ TestVideo/App.js
@@ -1,35 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
-import MediaPlayerView from './library/index';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity, Platform } from 'react-native';
+import MediaPlayerView, {Downloader} from './library/index';
 const videoUri = `https://d2h2jy22itvgms.cloudfront.net/${
     Platform.OS === 'ios' ? 'hls' : 'dash'
     }/269149/trailer.${Platform.OS === 'ios' ? 'm3u8' : 'mpd'}`;
+const {width, height} = Dimensions.get('window');
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-          <MediaPlayerView
-              ref={(ref) => {
-                  this.player = ref
-              }}
-              style={{width: 200, height: 150, backgroundColor: 'black'}}
-              autoplay={true}
-              preload='auto'
-              loop={true}
-              muted={false}
-              src={videoUri}
-          />
-      </View>
-    );
-  }
+    constructor(props){
+        super(props);
+
+        this.state = {
+            muted: false,
+            width: width,
+            height: width / (16 / 9),
+            showPlayer: false,
+            progress: 0,
+            play: false
+        };
+
+        this.onDownloadProgress = this.onDownloadProgress.bind(this);
+        this.downloader = new Downloader({
+            onDownloadProgress: this.onDownloadProgress,
+            onDownloadError: (data) => console.log(data),
+            onDownloadStarted: (data) => console.log(data),
+            onDownloadFinished: (data) => console.log(data),
+            onDownloadCanceled: (data) => console.log(data)
+        });
+        console.log('Downloader',this.downloader);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity onPress={() => {this.downloader.downloadStream('https://d2h2jy22itvgms.cloudfront.net/hls/269149/trailer.m3u8', '269149')}}>
+                    <Text>setupAssetDownload()</Text>
+                </TouchableOpacity>
+                <Text>{ this.state.progress}%</Text>
+            </View>
+        );
+    }
+
+    renderVideo() {
+        return (
+            <View style={styles.container}>
+                <MediaPlayerView
+                    ref={(ref) => {
+                        this.player = ref
+                    }}
+                    style={{width: 200, height: 150, backgroundColor: 'black'}}
+                    autoplay={true}
+                    preload='auto'
+                    loop={true}
+                    muted={false}
+                    src={videoUri}
+                />
+            </View>
+        );
+    }
+
+    onDownloadProgress(data) {
+        console.log(data);
+        this.setState({progress:data.percentComplete});
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
---
+++ TestVideo/android/download-service/src/main/java/com/example/download_service/NativeDownloadService.java
@@ -0,0 +1,74 @@
+package com.example.download_service;
+
+import android.app.Notification;
+
+import com.google.android.exoplayer2.offline.DownloadManager;
+import com.google.android.exoplayer2.offline.DownloadManager.TaskState;
+import com.google.android.exoplayer2.offline.DownloadService;
+import com.google.android.exoplayer2.scheduler.PlatformScheduler;
+import com.google.android.exoplayer2.ui.DownloadNotificationUtil;
+import com.google.android.exoplayer2.util.NotificationUtil;
+import com.google.android.exoplayer2.util.Util;
+
+public class NativeDownloadService extends DownloadService {
+
+    private static final String CHANNEL_ID = "download_channel";
+    private static final int JOB_ID = 1;
+    private static final int FOREGROUND_NOTIFICATION_ID = 1;
+
+    public NativeDownloadService() {
+        super(
+                FOREGROUND_NOTIFICATION_ID,
+                DEFAULT_FOREGROUND_NOTIFICATION_UPDATE_INTERVAL,
+                CHANNEL_ID,
+                R.string.exo_download_notification_channel_name);
+    }
+
+    @Override
+    protected DownloadManager getDownloadManager() {
+        return ((BlackDownloadApplication) getApplication()).getDownloadManager();
+    }
+
+    @Override
+    protected PlatformScheduler getScheduler() {
+        return Util.SDK_INT >= 21 ? new PlatformScheduler(this, JOB_ID) : null;
+    }
+
+    @Override
+    protected Notification getForegroundNotification(TaskState[] taskStates) {
+        return DownloadNotificationUtil.buildProgressNotification(
+                /* context= */ this,
+                R.drawable.exo_controls_play,
+                CHANNEL_ID,
+                /* contentIntent= */ null,
+                /* message= */ null,
+                taskStates);
+    }
+//
+//    @Override
+//    protected void onTaskStateChanged(TaskState taskState) {
+//        if (taskState.action.isRemoveAction) {
+//            return;
+//        }
+//        Notification notification = null;
+//        if (taskState.state == TaskState.STATE_COMPLETED) {
+//            notification =
+//                    DownloadNotificationUtil.buildDownloadCompletedNotification(
+//                            /* context= */ this,
+//                            R.drawable.exo_controls_play,
+//                            CHANNEL_ID,
+//                            /* contentIntent= */ null,
+//                            Util.fromUtf8Bytes(taskState.action.data));
+//        } else if (taskState.state == TaskState.STATE_FAILED) {
+//            notification =
+//                    DownloadNotificationUtil.buildDownloadFailedNotification(
+//                            /* context= */ this,
+//                            R.drawable.exo_controls_play,
+//                            CHANNEL_ID,
+//                            /* contentIntent= */ null,
+//                            Util.fromUtf8Bytes(taskState.action.data));
+//        }
+//        int notificationId = FOREGROUND_NOTIFICATION_ID + 1 + taskState.taskId;
+//        NotificationUtil.setNotification(this, notificationId, notification);
+//    }
+}
