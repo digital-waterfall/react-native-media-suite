d4130f36af544ef68b81be672bbff45afcc991c4
Craig van Heerden <craig.vanheerden@entelect.co.za>
2019-02-02 12:30:12 +0200

    Added ResizeMode to package exports

--- TestVideo/library/index.js
+++ TestVideo/library/index.js
@@ -3,6 +3,7 @@
 import MediaPlayerView from './media-player/media-player-view';
 import DownloadManager from './media-downloader/download-manager';
 import Download, { DOWNLOAD_STATES, EVENT_LISTENER_TYPES } from './media-downloader/download';
+import RESIZE_MODES from './media-player/VideoResizeMode';
 
 export default MediaPlayerView;
-export { DownloadManager, Download, DOWNLOAD_STATES, EVENT_LISTENER_TYPES };
+export { DownloadManager, Download, DOWNLOAD_STATES, EVENT_LISTENER_TYPES, RESIZE_MODES };
